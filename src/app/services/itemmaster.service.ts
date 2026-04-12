92729d6b40aa5c9b360332d3bb3dbd5d6c865775
Akash222005 <akashnatarajan10@gmail.com>
2026-04-12 19:41:41 +0530

    jwt code added successfully

--- src/app/services/itemmaster.service.ts
+++ src/app/services/itemmaster.service.ts
@@ -1,15 +1,38 @@
 import {Injectable,signal} from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Itemmaster } from "../models/itemmaster";
+import { PagedResult }from "../models/paged-result";
  
 @Injectable({providedIn:'root'})
  
 export class ItemmasterService
 {
-    private apiUrl="http://localhost:5234/api/ItemMaster";
+    private apiUrl="http://localhost:5234/api/v1/ItemMaster";
     items = signal<Itemmaster[]>([]);
     constructor(private http:HttpClient){}
+    getPagedItems(
+    catCode: string,
+    itemName: string,
+    uom: string,
+    pageNumber: number,
+    pageSize: number
+
+    ):Observable<PagedResult<Itemmaster>> {
+    let params = new HttpParams()
+      .set('catCode', catCode || '')
+      .set('itemName', itemName || '')
+      .set('uom', uom || '')
+      .set('pageNumber', pageNumber)
+      .set('pageSize', pageSize);
+ 
+    return this.http.get<PagedResult<Itemmaster>>(
+      `${this.apiUrl}/GetAllPaged`,
+      { params }
+    );
+  }
+
+
     GetAll():Observable<Itemmaster[]>
     {
         return this.http.get<Itemmaster[]>(`${this.apiUrl}/GetAll`);
